d484efc38f305e1f3a18869ca360e98e605b7671
Luis Enrique Guzman Pineda <luis.guzp13@gmail.com>
2022-08-23 16:38:20 -0500

    sign up user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :subscriptions, through: :bookings
-  PASSWORD_FORMAT = /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/
+  # PASSWORD_FORMAT = /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/
   validates :first_name, :last_name, :address, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, format: { with: PASSWORD_FORMAT }
+  # validates :password, presence: true, format: { with: PASSWORD_FORMAT }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
