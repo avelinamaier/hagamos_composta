ab7b603500d472b2dcd5787f602cb375f5e50693
Luis Enrique Guzman Pineda <luis.guzp13@gmail.com>
2022-08-25 12:27:28 -0500

    add zip code street city state and phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_210837) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_153200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_210837) do
     t.string "address"
     t.boolean "admin", default: false
     t.boolean "employee", default: false
+    t.string "zip_code"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.boolean "pick_up_status", default: false
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
