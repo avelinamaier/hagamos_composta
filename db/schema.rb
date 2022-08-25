2b72dd9e376637a8d70382fb079218f8cff905c5
Francisco Sotelo <franciscosotelov@gmail.com>
2022-08-25 16:56:27 -0500

    fix navbar user calls

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_08_25_192143) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,10 +82,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_192143) do
     t.index ["subscription_id"], name: "index_users_on_subscription_id"
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookings", "subscriptions"
   add_foreign_key "bookings", "users"
   add_foreign_key "users", "subscriptions"
 end
